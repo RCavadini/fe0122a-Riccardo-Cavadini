355ea30261e96345f53a81bd1ea0c506ff81a946
RCavadini <cava1991@gmail.com>
2022-02-24 21:11:22 +0100

    ProgettoS5

---
+++ s5/progetto/myScript.js
@@ -0,0 +1,90 @@
+buttonAdd = document.getElementById("addTask");
+creaHTML()
+
+buttonAdd.addEventListener("click",function(){
+let  inputTask = document.getElementById("inputTask").value
+let newTaskPosition = document.getElementById("TaskList")
+var li = document.createElement("li")
+li.innerHTML=inputTask
+li.classList.add("item")
+li.addEventListener("click",function(){
+    li.remove()
+    rimuoviElementodaDb(li)
+} )
+newTaskPosition.appendChild(li)
+salvaDatiNelDb(inputTask)
+
+
+
+})
+
+function creaHTML(){
+let newTaskPosition = document.getElementById("TaskList")    
+var elementiSalvati =JSON.parse(localStorage.getItem("elementiSalvati"))
+elementiSalvati.forEach(function(elemento){
+var li = document.createElement("li")
+li.innerHTML=elemento
+li.classList.add("item")
+li.addEventListener("click",function(){
+    li.remove()
+    rimuoviElementodaDb(li)
+} )
+newTaskPosition.appendChild(li)
+})
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ //con questa funzione salvo e aggiorno i dati in localstorage
+ function salvaDatiNelDb(inputTask){
+
+    /*salvataggio dati*/
+    
+        /* versione estesa
+        if( localStorage.getItem('elementiSalvati') == null ){
+            var db = [aggiunta]
+        }else{
+        var db = JSON.parse(localStorage.getItem('elementiSalvati'))
+        db.push(aggiunta)
+    }
+    */
+
+    var ls = localStorage.getItem('elementiSalvati')
+    
+    var db = ls ? JSON.parse(ls) : []
+    db.push(inputTask)
+    
+    localStorage.setItem('elementiSalvati',JSON.stringify(db))
+}
+
+//con questa funzione rimuovo un elemento da localstorage
+function rimuoviElementodaDb(el){
+
+    let ls = localStorage.getItem('elementiSalvati')
+    let db = JSON.parse(ls)
+
+    let indiceElemento = db.indexOf(el.innerHTML)
+
+    db.splice(indiceElemento,1)
+
+    localStorage.setItem('elementiSalvati',JSON.stringify(db))
+}
---
+++ s5/progetto/scriptDelloStarter.js
@@ -0,0 +1,34 @@
+ //con questa funzione salvo e aggiorno i dati in localstorage
+ function salvaDatiNelDb(aggiunta){
+
+    /*salvataggio dati*/
+    
+        /* versione estesa
+        if( localStorage.getItem('elementiSalvati') == null ){
+            var db = [aggiunta]
+        }else{
+        var db = JSON.parse(localStorage.getItem('elementiSalvati'))
+        db.push(aggiunta)
+    }
+    */
+
+    var ls = localStorage.getItem('elementiSalvati')
+    
+    var db = ls ? JSON.parse(ls) : []
+    db.push(aggiunta)
+    
+    localStorage.setItem('elementiSalvati',JSON.stringify(db))
+}
+
+//con questa funzione rimuovo un elemento da localstorage
+function rimuoviElementodaDb(el){
+
+    let ls = localStorage.getItem('elementiSalvati')
+    let db = JSON.parse(ls)
+
+    let indiceElemento = db.indexOf(el.innerHTML)
+
+    db.splice(indiceElemento,1)
+
+    localStorage.setItem('elementiSalvati',JSON.stringify(db))
+}
